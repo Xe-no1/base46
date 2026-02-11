67b78fc7a13788a2fae10d57c4f4447323c2ad3f
Xe-no1 <mazentech10@gmail.com>
2026-02-11 22:47:20 +0300

    feat(integrations): Add highlight definitions for 'mini-icons'

---
+++ lua/base46/integrations/mini-icons.lua
@@ -0,0 +1,50 @@
+local colors = require("base46").get_theme_tb "base_30"
+
+local highlights = {
+  MiniIconsAzure = {
+    fg = colors.blue,
+    bg = "NONE",
+  },
+
+  MiniIconsBlue = {
+    fg = colors.blue,
+    bg = "NONE",
+  },
+
+  MiniIconsCyan = {
+    fg = colors.cyan,
+    bg = "NONE",
+  },
+
+  MiniIconsGreen = {
+    fg = colors.green,
+    bg = "NONE",
+  },
+
+  MiniIconsGrey = {
+    fg = colors.white,
+    bg = "NONE",
+  },
+
+  MiniIconsOrange = {
+    fg = colors.orange,
+    bg = "NONE",
+  },
+
+  MiniIconsPurple = {
+    fg = colors.purple,
+    bg = "NONE",
+  },
+
+  MiniIconsRed = {
+    fg = colors.red,
+    bg = "NONE",
+  },
+
+  MiniIconsYellow = {
+    fg = colors.yellow,
+    bg = "NONE",
+  },
+}
+
+return highlights
